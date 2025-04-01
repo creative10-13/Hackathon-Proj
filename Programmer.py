9da8defd9b4014493a21d4bef6be1855702f8b91
creative10-13 <ronakparmar2010@gmail.com>
2025-04-01 11:41:18 -0400

    Adds Functions and my job

---
+++ Programmer.py
@@ -0,0 +1,58 @@
+from _Functions import slow_print
+
+def start_day():
+    slow_print("You wake up to your alarm. It's time to start your day as a programmer!")
+    choice = input("Do you: \n1. Hit snooze \n2. Get up immediately\n> ")
+    if choice == "1":
+        slow_print("You slept in and now you're late! You rush to your computer.")
+        return -10
+    else:
+        slow_print("You feel fresh and ready to start coding.")
+        return 10
+
+def breakfast():
+    slow_print("Time for breakfast!")
+    choice = input("Do you: \n1. Make a healthy breakfast \n2. Just grab coffee\n> ")
+    if choice == "1":
+        slow_print("Good choice! Your brain will thank you later.")
+        return 10
+    else:
+        slow_print("Caffeine is great, but you're running on fumes.")
+        return -5
+
+def coding_session():
+    slow_print("Time to start coding! What do you focus on?")
+    choice = input("Do you: \n1. Work on your feature \n2. Procrastinate on social media\n> ")
+    if choice == "1":
+        slow_print("You're making great progress and feeling accomplished!")
+        return 20
+    else:
+        slow_print("Oops, an hour just disappeared...and now you feel guilty.")
+        return -10
+
+def bug_fix():
+    slow_print("A critical bug appears! How do you handle it?")
+    choice = input("Do you: \n1. Debug systematically \n2. Panic and start rewriting code randomly\n> ")
+    if choice == "1":
+        slow_print("Nice! You solved it efficiently.")
+        return 15
+    else:
+        slow_print("Oh no, now things are worse than before!")
+        return -15
+
+def end_day(score):
+    slow_print("The day is over. Let's see how you did...")
+    if score > 30:
+        slow_print("You had a super productive day! Time to relax.")
+    elif score > 0:
+        slow_print("Not bad! A solid day of coding.")
+    else:
+        slow_print("Rough day...but there's always tomorrow!")
+    
+if __name__ == "__main__":
+    score = 0
+    score += start_day()
+    score += breakfast()
+    score += coding_session()
+    score += bug_fix()
+    end_day(score)
